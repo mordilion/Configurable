2d24afed34a11cca8a7a97a95aec978f9a944c3b
Henning Huncke <mordilion@gmx.de>
2018-09-25 15:46:18 +0200

    switched to an easier logic

--- src/Mordilion/Configurable/Configurable.php
+++ src/Mordilion/Configurable/Configurable.php
@@ -49,7 +49,7 @@ trait Configurable
             $config->merge($configuration);
         } 
 
-        if (!$config instanceof ConfigurationInterface) {
+        if ($config === null) {
             $config = $configuration;
         }
 
